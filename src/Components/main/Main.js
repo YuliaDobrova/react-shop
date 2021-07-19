bbdccce148418dfbfa5cce20b990f2f15f5d81c4
Yulia Dobrova <dobrova2009@gmail.com>
2021-07-19 22:15:57 +0300

    section

--- src/Components/main/Main.js
+++ src/Components/main/Main.js
@@ -2,12 +2,17 @@ import React from "react";
 import data from "../../data";
 import PhoneList from "../phoneList/PhoneList";
 import LaptopList from "../laptopList/LaptopList";
+import Section from "../section/Section";
 
 const Main = () => {
   return (
     <>
-      <PhoneList phones={data.phones} />
-      <LaptopList laptops={data.laptops} />
+      <Section title={"Мобильные телефоны"}>
+        <PhoneList phones={data.phones} />
+      </Section>
+      <Section title={"Ноутбуки"}>
+        <LaptopList laptops={data.laptops} />
+      </Section>
     </>
   );
 };
